d5941ab2af48a66b2e4b5aa478829cf805727be1
sshristi <ss.shristi.2411@gmail.com>
2020-05-15 13:46:05 +0530

    modified problem 1

--- Problem1.java
+++ Problem1.java
@@ -12,26 +12,25 @@ public class Problem1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n=0;
-		System.out.println("Enter size of the array: ");
+		
+		System.out.println("Enter size of the array: ");     //size of array
 		n = sc.nextInt();
-		System.out.println("Enter Array elements: ");
+		
+		System.out.println("Enter Array elements: ");        //enter data in array
 		List<Integer> l = new ArrayList<Integer>();
 		for(int q=0;q<n;q++)
 			l.add(sc.nextInt());
-		Map<Integer, Integer> map = l.parallelStream()
-					.collect(Collectors.toConcurrentMap(w -> w, w -> 1, Integer :: sum))
-					.entrySet()
-					.stream()
-					.sorted(Map.Entry.comparingByValue())
-					.collect(Collectors.toMap(Map.Entry::getKey,
-				               Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-		l.clear();
-		map.forEach((k,v) ->{
-			while(v-->0) {
-				l.add(k);
-			} 
-		} );
+	
+		System.out.println("Enter a no. from the list: ");    //no. x whose occurances to be added at the last
+		int x = sc.nextInt();
+		int count =(int) l.stream()							  // getting count of x in list l
+					.filter(e -> e==x)
+					.count();
 		
+		l.removeIf(e->e==x);                                 // removing x from previous positions
+		
+		while(count-->0)									//adding at last; occurances of x
+			l.add(x);
 		
 		System.out.println(l);
 		
